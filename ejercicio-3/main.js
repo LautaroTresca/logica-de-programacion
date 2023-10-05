55643fa64c792a2c588c4b0a6f7a3d2eb0e4ee79
Lautaro Tresca <lautarotrescadesarrollador@gmail.com>
2023-10-05 16:13:33 -0300

    ejercicio-3

--- ejercicio-3/main.js
+++ ejercicio-3/main.js
@@ -1,4 +1,5 @@
 // 3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
+
 const texto = document.getElementById("texto");
 const caracter = document.getElementById("caracter");
 const boton = document.getElementById("boton");
